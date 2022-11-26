6496170a95e298026cc2cd672580fcf0786148cc
satorik15 <sashator15@yandex.ru>
2022-11-26 14:00:26 +0300

    attempt to make ssh console version with name of file

--- ssh_controller.py
+++ ssh_controller.py
@@ -1,14 +1,19 @@
 import subprocess
 import re
 import paramiko
-class SSHController:
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname="10.129.0.162", username="admin", password="adb@2015", port=22)
-    ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("hdfs fsck /DATA/ -files -blocks -racks")
-    opt = ssh_stdout.readlines()
-    opt = "".join(opt)
-    opt1=re.findall("\[([^\[\]]*)\]",opt)[0]
-    opt2=opt1.split(", ")
-    print(opt2)
-    client.close()
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname="10.129.0.162", username="admin", password="adb@2015", port=22)
+name=input()
+stdin, stdout, stderr = client.exec_command(f"hadoop fs -find / -name {name}")
+opt1 = stdout.readlines()
+opt1 = "".join(opt1).strip()
+print(opt1)
+ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command(f"hdfs fsck {opt1} -files -locations -blocks -racks")
+opt = ssh_stdout.readlines()
+opt = "".join(opt)
+print(opt)
+opt3=re.findall("\[([^\[\]]*)\]",opt)
+print(opt3)
+client.close()
+
